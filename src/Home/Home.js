d2982aa75127626838ca21d22d418de90f434e25
Ujanmanandhar <umdr98@gmail.com>
2020-09-06 20:54:30 +0545

    nav-bar

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,22 @@
 import React from "react"
+import "./Home.css"
+
 
 function Home(){
     return(
-        <div>
-                
-        </div>
+                <header className="app-head">
+                    <div className="Main">
+                    <div className="Logos">
+                       <h1>Kathmandu</h1>
+                    </div>
+                    <nav className="Navbar">
+                    <a href="">Home</a>
+                    <a href="">Food</a>
+                    <a href="">Entertainment</a>
+                    <a href="">Travel</a>
+                    </nav>
+                    </div>
+                </header>
     )
 }
 
