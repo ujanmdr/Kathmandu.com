ecc584b3091584423c6eed8a9eee4d9f19a9302a
Ujanmanandhar <umdr98@gmail.com>
2020-09-09 19:02:30 +0545

    navbar complete

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -9,11 +9,30 @@ function Home(){
                     <div className="Logos">
                        <h1>Kathmandu</h1>
                     </div>
-                    <nav className="Navbar">
-                    <a href="">Home</a>
-                    <a href="">Food</a>
-                    <a href="">Entertainment</a>
-                    <a href="">Travel</a>
+                <nav className="Navbar">
+                    <ul className="nav-list">
+                        <li><a href="#">Home</a></li>
+                        <li class="dropdown"><a href="#">Food</a>
+                            <div class="submenu">
+                                <a href="#">Local Restaurant</a>
+                                <a href="#">Chill Places</a>
+                                <a href="#">Fancy Restaurant</a>
+                            </div>
+                        </li>
+                        <li class="dropdown"><a href="#">Culture</a>
+                            <div class="submenu">
+                                <a href="#">Festival</a>
+                                <a href="#">Ethic Groups</a>
+                            </div></li>
+                        <li class="dropdown"><a href="#">Travel</a>
+                            <div class="submenu">
+                                <a href="#">Trekking</a>
+                                <a href="#">Hiking</a>
+                                <a href="#">Safari</a>
+                                <a href="#">Picnic</a>
+                            </div>
+                            </li>
+                    </ul>
                     </nav>
                     </div>
                 </header>
