1a219e9423656fbf39c874d0471f7a557b82c1d4
Ujanmanandhar <umdr98@gmail.com>
2020-09-09 21:07:22 +0545

    home done

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react"
 import "./Home.css"
+import Kath from "../Assets/kath.jpg"
+import Sigal from "../Assets/sigal.jpg"
 
 
 function Home(){
     return(
+        <div >
                 <header className="app-head">
                     <div className="Main">
                     <div className="Logos">
@@ -36,6 +39,21 @@ function Home(){
                     </nav>
                     </div>
                 </header>
+                <div className="body-intro">
+                <div className="intro">
+                    <h1>Introduction</h1>
+                    <span class="line"></span>
+                    <p>/ˌkætmænˈduː/<br /> Nepali: काठमाडौँ, Nepali pronunciation: [ˈkaʈʰmaɳɖu] <br /> The capital and largest city of Nepal, with a population of around 1 million.<br />
+                     Also known as the city of temples, the city stands at an elevation of approximately 1,400 metres (4,600 feet)<br /> Above sea level in the bowl-shaped Kathmandu valley in central Nepal.</p>
+                     <img className="sigal"src={Sigal}></img>
+                </div>
+                <div>
+                <img className="Kath" src={Kath}></img>
+                <span className="Kathmandu">"The valley was historically called the "Nepal Mandala" and has been the home of the Newar people,<br /> a cosmopolitan urban civilization in the Himalayan foothills."</span>
+                </div>
+                </div>
+                </div>
+               
     )
 }
 
